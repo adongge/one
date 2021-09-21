4d1bcc6ce71da030eac30476f6a90bac402cb3b8
adong <adddge@sohu.com>
2021-09-21 21:19:32 +0800

    配置文件bug修复

--- config/one.php
+++ config/one.php
@@ -1,85 +1,87 @@
 <?php
 
 return [
-   'except' => [//排除的表
-      
+   'except' => [ //排除的表
+
    ],
    'list' => [
-      'table'        => 'adong_table',
-      'model'        => 'App\Models\AdongTable',
-      'controller'   => 'App\Admin\Controllers\AdongTableController',
-      'repository'     => '',
-      'migration'    => '',
-      'migrate'      => '',
-      'primary_key'  => 'id',
-      'timestamps'   => 1,
-      'soft_deletes' => 1,
-      'lang'         => 1,
-      'fields' => [
-         [
-            'name' => 'id',
-            'type' => 'unsignedBigInteger',
-            'key' => 'PRI',
-            'nullable' => 'NO',
-            'comment' => '',
-            'default' => ''
-         ],
-         [
-            'name' => 'parent_id',
-            'type' => 'bigInteger',
-            'key' => '',
-            'nullable' => 'NO',
-            'comment' => '',
-            'default' => '0'
-         ],
-         [
-            'name' => 'order',
-            'type' => 'integer',
-            'key' => '',
-            'nullable' => 'NO',
-            'comment' => '',
-            'default' => '0'
-         ],
-         [
-            'name' => 'title',
-            'type' => 'string',
-            'key' => '',
-            'nullable' => 'NO',
-            'comment' => '',
-            'default' => ''
-         ],
-         [
-            'name' => 'icon',
-            'type' => 'string',
-            'key' => '',
-            'nullable' => 'YES',
-            'comment' => '',
-            'default' => ''
-         ],
-         [
-            'name' => 'uri',
-            'type' => 'string',
-            'key' => '',
-            'nullable' => 'YES',
-            'comment' => '',
-            'default' => ''
-         ],
-         [
-            'name' => 'created_at',
-            'type' => 'timestamp',
-            'key' => '',
-            'nullable' => 'YES',
-            'comment' => '',
-            'default' => ''
-         ],
-         [
-            'name' => 'updated_at',
-            'type' => 'timestamp',
-            'key' => '',
-            'nullable' => 'YES',
-            'comment' => '',
-            'default' => ''
-         ],
+      [
+         'table'        => 'adong_table',
+         'model'        => 'App\Models\AdongTable',
+         'controller'   => 'App\Admin\Controllers\AdongTableController',
+         'repository'     => '',
+         'migration'    => '',
+         'migrate'      => '',
+         'primary_key'  => 'id',
+         'timestamps'   => 1,
+         'soft_deletes' => 1,
+         'lang'         => 1,
+         'fields' => [
+            [
+               'name' => 'id',
+               'type' => 'unsignedBigInteger',
+               'key' => 'PRI',
+               'nullable' => 'NO',
+               'comment' => '',
+               'default' => ''
+            ],
+            [
+               'name' => 'parent_id',
+               'type' => 'bigInteger',
+               'key' => '',
+               'nullable' => 'NO',
+               'comment' => '',
+               'default' => '0'
+            ],
+            [
+               'name' => 'order',
+               'type' => 'integer',
+               'key' => '',
+               'nullable' => 'NO',
+               'comment' => '',
+               'default' => '0'
+            ],
+            [
+               'name' => 'title',
+               'type' => 'string',
+               'key' => '',
+               'nullable' => 'NO',
+               'comment' => '',
+               'default' => ''
+            ],
+            [
+               'name' => 'icon',
+               'type' => 'string',
+               'key' => '',
+               'nullable' => 'YES',
+               'comment' => '',
+               'default' => ''
+            ],
+            [
+               'name' => 'uri',
+               'type' => 'string',
+               'key' => '',
+               'nullable' => 'YES',
+               'comment' => '',
+               'default' => ''
+            ],
+            [
+               'name' => 'created_at',
+               'type' => 'timestamp',
+               'key' => '',
+               'nullable' => 'YES',
+               'comment' => '',
+               'default' => ''
+            ],
+            [
+               'name' => 'updated_at',
+               'type' => 'timestamp',
+               'key' => '',
+               'nullable' => 'YES',
+               'comment' => '',
+               'default' => ''
+            ]
+         ]
       ]
-   ],
+   ]
 ];
